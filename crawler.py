b8cc72539c7cf80014b81f0a879510548a13d802
rarecard <62014039+rarecard@users.noreply.github.com>
2022-05-01 15:57:38 +0800

    Store the captured data in a database
    Connect to the database to store the captured data into the database

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pymysql
 
 # 获取数据
 def Get_json(url, headers):
@@ -9,19 +10,32 @@ def Get_json(url, headers):
     f.write(data)
     return req
 
-# 数据
+
+# 处理数据
 def screen(req):
+    connect = pymysql.connect(host='localhost',
+                              port=3306,
+                              user='root',
+                              passwd='123456',
+                              charset='utf8',
+                              )
+    cursor = connect.cursor()
+    cursor.execute("DROP DATABASE IF EXISTS pupu")
+    cursor.execute(" create database pupu")
+    cursor.execute("use pupu")
+    cursor.execute(
+        "create table products (name  VARCHAR (255) PRIMARY KEY,price  DECIMAL (6,3),spec  VARCHAR (255),origin   VARCHAR (255),sub_title VARCHAR(255))character set utf8 collate utf8_general_ci")
+    sql = "INSERT INTO products (name,peice,spec,origin,sub_title) VALUES ('%s',%.2f,'%s','%s','%s')"
+
     list = []
     for index in range(10):
         price = float(req[index]['price']) / 100
-        data = {'name': req[index]['name'],  # 商品名称
-                'spec': req[index]['spec'],  # 商品含量
-                'price': price,  # 价格
-                'title': req[index]['sub_title'],  # 详细信息
-                'origin':req[index]['origin']   #产地
-                }
-        list.append(data)
-    return list
+        data = (req[index]['name'], price, req[index]['spec'],req[index]['origin'], req[index]['sub_title'])
+        cursor.execute(sql % data)
+        connect.commit()
+     # # 关闭连接
+    cursor.close()
+    connect.close()
 
 if __name__ == '__main__':
     # 头部信息，用来模拟浏览器发送请求
@@ -34,8 +48,5 @@ if __name__ == '__main__':
         'Host': 'j1.pupuapi.com'
     }
     # json文件路径
-    url = "https://j1.pupuapi.com/client/marketing/storeproduct/v2/search?business=scenes&business_id=420&category_id=420&in_stock=-1&page=1&size=20&sort=0&store_id=7c1208da-907a-4391-9901-35a60096a3f9&tag=-1"   # 存储json内容输出
-    list = screen(Get_json(url, headers))
-
-    for i in range(10):
-        print(list[i])
+    url = "https://j1.pupuapi.com/client/marketing/storeproduct/v2/search?business=scenes&business_id=420&category_id=420&in_stock=-1&page=1&size=20&sort=0&store_id=7c1208da-907a-4391-9901-35a60096a3f9&tag=-1"  # 存储json内容输出
+    screen(Get_json(url, headers))
